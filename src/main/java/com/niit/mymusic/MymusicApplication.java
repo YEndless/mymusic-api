e7bf451e3526a13ca176f032f68fd52ead4e00ce
YEndless <895315416@qq.com>
2018-11-18 22:45:07 +0800

    mymusic后端

---
+++ src/main/java/com/niit/mymusic/MymusicApplication.java
@@ -0,0 +1,12 @@
+package com.niit.mymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymusicApplication.class, args);
+	}
+}
